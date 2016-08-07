1a2d1b27beb1343fd82e30df5af79080a563d92f
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-08-07 17:59:42 -0400

    Return the number of vowels in a string

---
+++ VowelCount.js
@@ -0,0 +1,18 @@
+/*
+Vowel Count
+Using the JavaScript language, have the function VowelCount(str)
+take the str string parameter being passed and return the number
+of vowels the string contains (ie. "All cows eat grass" would return 5).
+Do not count y as a vowel for this challenge. 
+*/
+
+function VowelCount(str) {
+  return str.match(/[aeiou]/gi).length;//match the vowels and return the string length      
+}
+
+console.log(VowelCount("hello"));
+/*
+Input = "hello"     Output = 2
+Input = "coderbyte" Output = 3
+*/
+Contact GitHub API Training Shop Blog About
